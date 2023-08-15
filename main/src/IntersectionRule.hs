978b5cc46e5caf0af605e733633f48506780f694
Shriya <142327150+SmoothThunk@users.noreply.github.com>
2023-08-15 18:48:48 -0400

    Added both correct and incorrect test cases

--- main/src/IntersectionRule.hs
+++ main/src/IntersectionRule.hs
@@ -37,10 +37,22 @@ main :: IO ()
 main = do
     let gamma = Extend EmptyContext "x" (TypeVar "A")
         m = Var "M"
-
+        n = Var "N"
+    
     putStrLn "Intersection typing rules:"
-    -- Applying type elimination and introduction rules
+    
+    -- Type elimination and introduction rules for intersection types
     putStrLn $ "Result 1: " ++ show (typeElimination gamma m (TypeVar "A") && typeElimination gamma m (TypeIntersection (TypeVar "A") (TypeVar "B")))
     putStrLn $ "Result 2: " ++ show (typeIntroduction gamma m (TypeIntersection (TypeVar "A") (TypeVar "B")))
-    -- Checking type introduction and elimination together
-    putStrLn $ "Result 3: " ++ show (gamma |- m : TypeVar "A" && Extend gamma "x" (TypeVar "A") |- m : TypeVar "B")
+    
+    -- Intersection introduction rule
+    putStrLn $ "Result 3: " ++ show (gamma |- m : TypeVar "A" && Extend gamma "x" (TypeVar "A") |- n : TypeVar "B" && gamma |- n : TypeIntersection (TypeVar "A") (TypeVar "B"))
+    
+    -- Intersection elimination rule
+    putStrLn $ "Result 4: " ++ show (gamma |- m : TypeIntersection (TypeVar "A") (TypeVar "B") && gamma |- m : TypeVar "A")
+    putStrLn $ "Result 5: " ++ show (gamma |- m : TypeIntersection (TypeVar "A") (TypeVar "B") && gamma |- m : TypeVar "B")
+    
+    -- Incorrect cases
+    putStrLn $ "Result 6: " ++ show (typeIntroduction gamma m (TypeVar "A"))  -- Should fail
+    putStrLn $ "Result 7: " ++ show (typeElimination gamma m (TypeVar "B"))  -- Should fail
+    putStrLn $ "Result 8: " ++ show (typeElimination gamma m (TypeIntersection (TypeVar "A") (TypeVar "C")))  -- Should fail
